65929c4112c1bd500efba9b48a3340dd87b4b65a
unknown <mauricedallo@gmail.com>
2026-03-05 01:32:03 +0800

    main script draft

---
+++ morning_briefing_mirror.py
@@ -0,0 +1,27 @@
+import datetime
+import os
+import requests
+from dotenv import load_dotenv
+
+#load environment
+load_dotenv()
+
+
+WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
+DEFAULT_LOCATION = "Manila, PH"
+MAX_NEWS_HEADLINES = 3
+
+def get_weather():
+    return
+
+def get_local_headlines():
+    return
+
+def get_daily_schedue():
+    return
+
+def generate_morning_briefing():
+    return
+
+if __name__ == "__main__":
+    generate_morning_briefing()
